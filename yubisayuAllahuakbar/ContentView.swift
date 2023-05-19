1a0729692cad9b95facfed664dbcfef2e7942ccf
syifa <syifanisha@gmail.com>
2023-05-19 13:55:01 +0700

    Initial Commit

---
+++ yubisayuAllahuakbar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  yubisayuAllahuakbar
+//
+//  Created by Asyifa Tasya Fadilah on 19/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
